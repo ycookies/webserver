e4c48189b4a73f77f95b7ecbaa1cededaf44d07a
eRic <3664839@qq.com>
2018-07-08 22:05:29 +0800

    'one01'

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,27 @@
 <?php
-
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
+/*Route::get('/user', function (Request $request) {
+    return response()->json([
+        'error' => 'Resource not found.'
+    ],500);
+})->middleware('auth:api');*/
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    echo "api-server";
+});
+
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+    $api->group(['namespace' => 'App\Api\V1\Controllers'], function ($api) {
+        $api->any('ArticleIndex', 'ArticleController@index');
+        $api->any('ArticleShow', 'ArticleController@show');
+    });
+});
+
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ar', function () {
+    return response()->json(['code'=>100],200);
+});
